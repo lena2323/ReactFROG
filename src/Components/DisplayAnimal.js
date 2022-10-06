9410e800118a6e22211297307256b4531da5f67d
Lena2323 <lena.jovicic23@gmail.com>
2022-10-06 15:41:33 +0200

    what am i doing

--- src/Components/DisplayAnimal.js
+++ src/Components/DisplayAnimal.js
@@ -7,47 +7,60 @@ const DisplayAnimal = () => {
   const [data, setData] = useState({data: []});
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
+  const [yyy, setYYY] = useState('');
+  
 
   const handleClick = async () => {
     setIsLoading(true);
 
     try {
+
       const response = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand'
       );
+
+      
 
       if (!response.ok) {
         throw new Error(`Error! status: ${response.status}`);
       }
 
       const result = await response.json();
-      
-
-
+      console.log(data);
       setData(result);
+
+      const ehe = 
+      <div className="animalContainer">
+      <img src={data.image_link} />
+      <p>My name is {data.name}</p>
+      <p>I am a {data.animal_type}</p>
+      <p>Am I cute or what?</p>
+      </div>
+
+      setYYY(ehe);
+
+
+
     } catch (err) {
       setErr(err.message);
     } finally {
       setIsLoading(false);
-    }
+    };
+
+    
   };
 
-  console.log(data);
 
   return (
+
     <div>
       {err && <h2>{err}</h2>}
-
       <button onClick={handleClick}>Fetch data</button>
-
       {isLoading && <h2>Loading...</h2>}
+      {yyy}
 
-        <div className="animalContainer">
-        <img src={data.image_link} />
-        <p>My name is {data.name}</p>
-        <p>I am a {data.animal_type}</p>
-        <p>Am I cute or what?</p>
-        </div>
     </div>
+    
+
   );
 };
 
