59a69e784fdfa2bd470d3e1d03164c8adbb8df69
Lena2323 <lena.jovicic23@gmail.com>
2022-10-06 18:43:17 +0200

    messing with useState and I actually understand less

--- src/Components/DisplayAnimal.js
+++ src/Components/DisplayAnimal.js
@@ -4,8 +4,9 @@ const DisplayAnimal = () => {
   const [data, setData] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
-  const [yyy, setYYY] = useState('');
-  
+  const [animalInfo, setanimalInfo] = useState('');
+  const [message, setMessage] = useState('');
+
   const handleClick = async () => {
     setIsLoading(true);
 
@@ -22,14 +23,36 @@ const DisplayAnimal = () => {
       console.log(result);
       setData(result);
 
-      const ehe = 
+      const animalInfoContents = 
       <div className="animalContainer">
       <img src={result.image_link} />
       <p>My name is {result.name}</p>
       <p>I am a {result.animal_type}</p>
       <p>Am I cute or what?</p>
       </div>
-      setYYY(ehe);
+
+
+      if (result.animal_type == 'Mammal'){
+        setMessage("I heard there's milk missing in shops in yer country.... Milk this! *shows middle finger*") 
+      }
+
+      if (result.animal_type == 'Bird'){
+        setMessage("If you want to fly like me try jumping off a  building!") 
+      }
+      if (result.animal_type == 'Reptile'){
+        setMessage("Am I ugly? Yes! Am I scary? Also yes! And so are you, wooohooo!") 
+      }
+
+      if (result.animal_type == 'Amphibian'){
+        setMessage("I eat flys and still live, you can survive without the cooker too!") 
+      }
+      
+      if (result.animal_type == 'Fish'){
+        setMessage("Water is my best friend and I ain't got no kidney stones!") 
+      }
+
+
+      setanimalInfo(animalInfoContents);
 
     } catch (err) {
       setErr(err.message);
@@ -47,8 +70,10 @@ const DisplayAnimal = () => {
 
       <button onClick={handleClick} className="universal-button">Fetch data</button>
       {isLoading && <h2>Loading...</h2>}
-      {yyy}
-
+      {animalInfo} 
+      <div className='inspirational-message-container'>
+      Inspirational message: <br></br> {message}
+      </div>
     </div>
     
   );
